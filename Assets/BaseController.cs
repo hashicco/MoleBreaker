ea7d395b05bc2fb666836793e2a2ba30a2acac9b
hashicco <hashicco.dev@gmail.com>
2018-03-13 21:14:29 +0900

    シーン構成中

---
+++ Assets/BaseController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour {
+	public static int current_score = 0;
+	public static int high_score = 0;
+	public static int[] top_scores = new int[10];
+
+	// Use this for initialization
+	void Start () {
+//		DontDestroyOnLoad(this);
+		Application.LoadLevelAdditive("Title");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
